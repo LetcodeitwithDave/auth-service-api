5ae762290cb5af494c8674cb9611750b928ebb42
David <sololadavid4@gmail.com>
2026-05-08 18:48:32 +0100

    implement error handling and graceful shutdown in server

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import { connectDb, disconnectDb } from "./src/config/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+connectDb();
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
+});
+
+// measures inplace to prevent data loss when code breaks:
+// Handle unhandled promise rejections (e.g... database connections error)
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection:", err);
+  server.close(async () => {
+    await disconnectDb();
+    process.exit(1);
+  });
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", async (err) => {
+  console.log("Uncaught Exception:", err);
+  await disconnectDb();
+  process.exit(1);
+});
+
+// Graceful shutdown
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received, shuttiing down gracefully");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
 });
