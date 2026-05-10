859718cd0eb04ece1d9e79f0be80b2cd38997c39
David <sololadavid4@gmail.com>
2026-05-10 20:18:04 +0100

    auth route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import authRoute from "./routes/authRoute.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// ROUTES
+app.use("/api/auth", authRoute);
 
 export default app;
